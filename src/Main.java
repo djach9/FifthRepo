584190cce3e1631d4148d2117adde992bba56886
djach <djach@sii.pl>
2023-03-01 21:32:50 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> properties = new HashMap<>();
+
+        properties.put("login", "Angelina");
+        properties.put("password", "angelinaInHollywood123");
+        properties.put("title", "Salt");
+        properties.put("url", "www.angelina.hollywood");
+        properties.put("errorMessage", "file has not been founded");
+
+        String[] sensitiveKeys = {"login","password","url"};
+
+
+        properties.forEach((key, value) -> {
+            if (Set.of(sensitiveKeys).contains(key)) {
+                System.out.println(key + ": ***");
+            } else {
+                System.out.println(key + ": " + value);
+            }
+        });
+
+    }
+}
